e941045d16425b21067bd1e8be34db5e4dddbbe7
Yevhenii <zheki2004@gmail.com>
2023-10-17 13:57:51 +0300

    react catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { NavBar } from './components/NavBar/NavBar';
 import './App.scss';
+import { Footer } from './components/Footer/Footer';
+import { HomePage } from './pages/HomePage/HomePage';
+import { Phone } from './types/Phone';
+import { getProducts } from './api';
+import { CartProvider } from './context/CartContext';
+import { FavouriteProvider } from './context/FavouriteContext';
+import { ProductPage } from './pages/1/ProductPage/ProductPage';
+import { ProductCardPage } from './pages/ProductCardPage/ProductCardPage';
+import { CartPage } from './pages/CartPage/CartPage';
+import { FavouritePage } from './pages/FavouritePage/FavouritePage';
 
-interface Props {
-  onClick: () => void;
-}
+const App: React.FC = () => {
+  const [products, setProducts] = useState<Phone[]>([]);
 
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+  const loadProducts = async () => {
+    try {
+      const productsFromServer = await getProducts();
 
-export const App: React.FC = () => {
+      setProducts(productsFromServer);
+    } catch {
+      throw new Error('Loading products error');
+    }
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="App">
+      <CartProvider>
+        <FavouriteProvider>
+          <NavBar />
+          <main className="main__content">
+            <Routes>
+              <Route path="/react_phone-catalog/">
+                <Route index element={<HomePage products={products} />} />
+
+                <Route path=":category">
+                  <Route
+                    index
+                    element={<ProductPage products={products} />}
+                  />
+
+                  <Route
+                    path=":productId"
+                    element={(<ProductCardPage products={products} />)}
+                  />
+                </Route>
+
+                <Route path="cart" element={<CartPage />} />
+                <Route path="favourite" element={<FavouritePage />} />
+              </Route>
+            </Routes>
+          </main>
+          <Footer />
+        </FavouriteProvider>
+      </CartProvider>
     </div>
   );
 };
+
+export default App;
